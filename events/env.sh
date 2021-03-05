4ca0fbc5a594d15acb46996be687a065ab0b6ebb
sanjdutt <sanjdutt8@gmail.com>
2021-03-05 21:49:19 +0000

    deploy

---
+++ events/env.sh
@@ -0,0 +1,6 @@
+export SECRET_KEY_BASE=W68eso5YQOlbtvSNUR50N/HDWj6IaEhAwMR3LtzuBEQAefwYVbX84bvoTA7XtiGi
+export MIX_ENV=prod 
+export PORT=4798
+export NODEBIN='pwd'/assets/node_modules/.bin
+export PATH="$PATH:$NODEBIN"
+export DATABASE_URL=ecto://events:Turtle201412@45.77.107.233/events_dev
