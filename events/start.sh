8009c303fb5e40f065ab41c3c939b551c56f7a91
sanjdutt <sanjdutt8@gmail.com>
2021-03-12 04:49:57 +0000

    first final

--- events/start.sh
+++ events/start.sh
@@ -1,19 +1,6 @@
-#!/bin/bash
-
-export MIX_ENV=prod
+export SECRET_KEY_BASE=W68eso5YQOlbtvSNUR50N/HDWj6IaEhAwMR3LtzuBEQAefwYVbX84bvoTA7XtiGi
+export MIX_ENV=prod 
 export PORT=4798
-
-CFGD=$(readlink -f ~/.config/events)
-
-if [ ! -e "$CFGD/base" ]; then
-    echo "run deploy first"
-    exit 1
-fi
-
-DB_PASS=$(cat "$CFGD/db_pass")
-export DATABASE_URL=ecto://events:$DB_PASS@localhost/events_dev
-
-SECRET_KEY_BASE=$(cat "$CFGD/base")
-export SECRET_KEY_BASE
-
-_build/prod/rel/events/bin/events start
+export NODEBIN='pwd'/assets/node_modules/.bin
+export PATH="$PATH:$NODEBIN"
+export DATABASE_URL=ecto://events:Turtle201412@45.77.107.233/events_dev
